4749ef7a04b3c8b09287c21a8c319ae2907b9eef
Miguel Miranda <miguel.miranda@sapo.pt>
2025-01-21 16:57:03 +0000

    [ejbca/test1-1] add new input choice algorithm to performance test workflow

--- tests/create_csr.sh
+++ tests/create_csr.sh
@@ -6,25 +6,25 @@ algorithm=$2
 subject=$3
 
 key_cmd=""
-key_algorithm=$(echo $algorithm | cut -d ':' -f1)
+key_algorithm=$(echo ${algorithm} | cut -d ':' -f1)
 
-if [[ "$key_algorithm" == "RSA" ]]; then
-    key_opt=$(echo $algorithm | cut -d ':' -f2)
+if [[ "${key_algorithm}" == "RSA" ]]; then
+    key_opt=$(echo ${algorithm} | cut -d ':' -f2)
     # Generate key
-    key_cmd="openssl genpkey -algorithm $key_algorithm -pkeyopt rsa_keygen_bits:$key_opt 2>/dev/null"
-elif [[ "$key_algorithm" == "EC" ]]; then
-    key_opt=$(echo $algorithm | cut -d ':' -f2)
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} -pkeyopt rsa_keygen_bits:${key_opt} 2>/dev/null"
+elif [[ "${key_algorithm}" == "EC" ]]; then
+    key_opt=$(echo ${algorithm} | cut -d ':' -f2)
     # Generate key
-    key_cmd="openssl genpkey -algorithm $key_algorithm -pkeyopt ec_paramgen_curve:$key_opt 2>/dev/null"
-elif [[ "$key_algorithm" == "ED25519" ]]; then
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} -pkeyopt ec_paramgen_curve:${key_opt} 2>/dev/null"
+elif [[ "${key_algorithm}" == "ED25519" ]]; then
     # Generate key
-    key_cmd="openssl genpkey -algorithm $key_algorithm 2>/dev/null"
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} 2>/dev/null"
 fi
-echo "$subject $algorithm $THREAD $key_cmd $key_algorithm " >> /home/guest/github_runner/actions-runner/_work/ejbca-test1/mm.txt
+echo "${subject} ${algorithm} ${THREAD} ${key_cmd} ${key_algorithm} " >> /home/guest/github_runner/actions-runner/_work/ejbca-test1/mm.txt
 # Create CSR
-csr_cmd="openssl req -new -key <($key_cmd) -subj /CN=enduser${THREAD}/O=enduser${THREAD}_org/C=PT"
+csr_cmd="openssl req -new -key <(${key_cmd}) -subj /CN=enduser${THREAD}/O=enduser${THREAD}_org/C=PT"
 #csr_cmd="openssl req -new -key <($key_cmd) -subj $subject"
 echo -n "-----BEGIN CERTIFICATE REQUEST-----\n"
-eval $csr_cmd | grep -v "CERTIFICATE REQUEST" | tr -d '\n'
+eval ${csr_cmd} | grep -v "CERTIFICATE REQUEST" | tr -d '\n'
 echo -n "\n-----END CERTIFICATE REQUEST-----"
 
